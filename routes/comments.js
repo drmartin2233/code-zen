c530bf97aff9de6c072ac39fb425be4c45259fd7
David Martin <d.r.martin.wrk@gmail.com>
2023-03-03 11:25:44 -0500

    banana

--- routes/comments.js
+++ routes/comments.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const commentsCtrl = require('../controllers/comments');
-const ensureLoggedIn = require('../config/ensureLoggedIn');
+const ensureLoggedIn = require('../config/banana');
 
 // All routes "starts with" / (root)
 
